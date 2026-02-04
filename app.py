d66dd910fa612a3cbb3cd21e032abfb38d21c45a
Jovan <najdanovskijovan@gmail.com>
2026-02-04 21:04:53 +0100

    Updated

--- app.py
+++ app.py
@@ -42,8 +42,7 @@ def home():
     return render_template("home.html")
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run()
 
 @app.route("/quiz", methods=["GET", "POST"])
 def quiz():
